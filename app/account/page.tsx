7ed544de636dd4fe37a9cad5879ed58f221d5ea5
Mathieu Lefebvre <mathieulefebvre@Mathieus-MacBook-Air.local>
2026-03-21 15:47:32 +0100

    fix: pass cardHistory prop to HistoryTab in account page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -342,7 +342,7 @@ export default function ComptePage() {
 
         {/* HISTORIQUE */}
         {tab === 'history' && (
-          <HistoryTab events={historyEvents} />
+          <HistoryTab events={historyEvents} cardHistory={[]} />
         )}
 
         {/* ACTIVITÉ BSKY */}
